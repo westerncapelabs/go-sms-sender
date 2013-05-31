856abc7bc1e019f7a7c60679c7e47256f4d93b53
Mike Jones <mike@westerncapelabs.com>
2013-05-31 10:48:52 +0200

    Trying multiple sends

--- test/test-go-sms-sender.js
+++ test/test-go-sms-sender.js
@@ -64,7 +64,7 @@ describe("When using the SMS sender via USSD", function() {
         p.then(done, done);
     });
 
-    it("we should be told SMS sent and asked if we want to do another", function (done) {
+    it.skip("we should be told SMS sent and asked if we want to do another", function (done) {
         var p = tester.check_state({
             user: null,
             content: "Hello Mike",
@@ -78,20 +78,67 @@ describe("When using the SMS sender via USSD", function() {
     });
 
     it("we should decline sending another and get an ending thankyou", function (done) {
-        var user = {
-            current_state: "second_state",
-            answers: {
-                first_state: "Hello Mike"
-            }
-        }
+        // var user = {
+        //     current_state: "fi",
+        //     answers: {
+        //         first_state: "Hello Mike"
+        //     }
+        // }
         var p = tester.check_state({
-            user: user,
-            content: "2",
+            user: null,
+            content: "Hello Mike",
             next_state: "end_state",
-            response: "^Thank you and bye bye!$",
+            response: "^You said 'Hello Mike'. Thank you and bye bye!$",
             continue_session: false
         });
         p.then(done, done);
     });
+
+});
+
+describe("When using the SMS sender via SMS", function() {
+
+    // These are used to mock API reponses
+    // EXAMPLE: Response from google maps API
+    var fixtures = [
+       'test/fixtures/example-geolocation.json'
+    ];
+
+    var tester = new vumigo.test_utils.ImTester(app.api, {
+        custom_setup: function (api) {
+            api.config_store.config = JSON.stringify({
+                sms_tag: ['pool', 'addr']
+                //user_store: "go_skeleton"
+            });
+            fixtures.forEach(function (f) {
+                api.load_http_fixture(f);
+            });
+        },
+        async: true
+    });
+
+    var assert_single_sms = function(to_addr, content) {
+        var teardown = function(api) {
+            var sms = api.outbound_sends[0];
+            assert.equal(api.outbound_sends.length, 1);
+            assert.equal(sms.to_addr, to_addr);
+            assert.equal(sms.content, content);
+        };
+        return teardown;
+    };
+
+    // first test should always start 'null, null' because we haven't
+    // started interacting yet
+    it.skip("say something to app should enter text and exit", function (done) {
+        var p = tester.check_state({
+            user: null,
+            content: "Hello Mike",
+            next_state: "end_state",
+            response: "^You said 'Hello Mike'. Thank you and bye bye!",
+            continue_session: false
+        });
+        p.then(done, done);
+    });
+
 
 });
